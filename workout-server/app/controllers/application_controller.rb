ff26f7a39c0ee62a0f42e16f8b3710da68f8cd5b
Zachary <zacharylampi96@gmail.com>
2022-08-02 15:07:47 -0400

    seeds data added for 5 groups, contollers with get post and delete for exc

--- workout-server/app/controllers/application_controller.rb
+++ workout-server/app/controllers/application_controller.rb
@@ -5,5 +5,32 @@ class ApplicationController < Sinatra::Base
     get "/" do
       { message: "Good luck with your project!" }.to_json
     end
-  
+
+    get "/GymBros" do
+     gymbro=Gymbro.all
+      gymbro.to_json
+    end
+
+    get "/Exercise" do
+      exercise=Exercise.all
+      exercise.to_json
+    end
+
+    delete "/Exercise/:id" do
+      deleteexc=Exercise.find(params[:id])
+      deleteexc.destroy
+    end
+
+    post "/Exercise" do
+    createexc=Exercise.create(exercise:params[:exercise], weight: 0, completed_workout: false, musclegroup: m1 ,img:params[:img],description: params[:description], )
+    createexc.to_json
+    end
+
+
+     get "/Musclegroup" do
+      musclegroup=Musclegroup.all
+      musclegroup.to_json
+     end
+
+     
   end
