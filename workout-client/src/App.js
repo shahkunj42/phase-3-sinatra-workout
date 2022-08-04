200723dbc5b738f70833e6cbf806ddfe223813b4
Kunj <shahkunj42@gmail.com>
2022-08-04 13:30:10 -0400

    prebranch

--- workout-client/src/App.js
+++ workout-client/src/App.js
@@ -4,29 +4,29 @@ import Exercise from './exercise'
 import MuscleGroups from './musclegroups';
 
 function App() {
-const [count, setCount] = useState(1);
+// const [count, setCount] = useState(1);
 const [gymBros, setGymBros] = useState([])
-const [tester, setTester] = useState([])
+// const [tester, setTester] = useState([])
 
 
-const increment = () => {
-  if (count<6) {setCount((prevCount) => prevCount + 1)}
-  else {setCount(1)}
-};
+// const increment = () => {
+//   if (count<6) {setCount((prevCount) => prevCount + 1)}
+//   else {setCount(1)}
+// };
 
 useEffect(() => {
-  fetch(`http://localhost:9292/GymBros/${count}`)
+  fetch(`http://localhost:9292/GymBros/1`)
   .then(r => r.json())
   .then(data => setGymBros(data))
-},[count])
+},[])
 
 // useEffect(() => {
 //   let renderMuscles = <MuscleGroups musclegroups = {gymBros.musclegroups}/>
 
 // //   return renderMuscles
 // // }, [gymBros])
-useEffect(()=> {setTimeout(() => setTester(gymBros.musclegroups[0].exercise.map( things => things.exercise)
-  ), 1000);},[gymBros])
+// useEffect(()=> {setTimeout(() => setTester(gymBros.musclegroups[0].exercise.map( things => things.exercise)
+//   ), 1000);},[gymBros])
 
   // useEffect(()=> {setTimeout(() => setTester(gymBros.musclegroups), 1000)},[gymBros])
 
@@ -37,15 +37,16 @@ useEffect(()=> {setTimeout(() => setTester(gymBros.musclegroups[0].exercise.map(
 // console.log(tester)
 
 // const renderlist = gymBros.musclegroups[0].exercise.map(name => <Exercise  name={name}/>)
-const putter = tester.map(things => things)
+// const putter = tester.map(things => things)
+
 return (
   <div>
-    <button onClick={increment}>Next Gym Rat{count}</button>
+    {/* <button onClick={increment}>Next Gym Rat{count}</button> */}
     <div>{gymBros.name}</div>
-    <div>{putter}</div>
+    {/* <div>{putter}</div> */}
     {/* <div>{gymBros.musclegroups[0].musclegroup}</div> */}
     {/* <div>{renderlist}</div> */}
-    {/* <MuscleGroups musclegroups={gymBros.musclegroups} /> */}
+    <MuscleGroups musclegroups={gymBros.musclegroups} />
     
   </div>
   );
