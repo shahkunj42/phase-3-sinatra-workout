64a08340e467ce02ed1a07d2795054537b68355c
Kunj <shahkunj42@gmail.com>
2022-08-04 12:00:27 -0400

    current

--- workout-client/src/App.js
+++ workout-client/src/App.js
@@ -23,16 +23,18 @@ useEffect(() => {
 // useEffect(() => {
 //   let renderMuscles = <MuscleGroups musclegroups = {gymBros.musclegroups}/>
 
-//   return renderMuscles
-// }, [gymBros])
+// //   return renderMuscles
+// // }, [gymBros])
 useEffect(()=> {setTimeout(() => setTester(gymBros.musclegroups[0].exercise.map( things => things.exercise)
   ), 1000);},[gymBros])
+
+  // useEffect(()=> {setTimeout(() => setTester(gymBros.musclegroups), 1000)},[gymBros])
 
 
 // setTimeout(() => {return (gymBros.musclegroups[0].exercise.map( things => things.exercise)
 // )}, 5000);
-console.log(gymBros)
-console.log(tester)
+// console.log(gymBros)
+// console.log(tester)
 
 // const renderlist = gymBros.musclegroups[0].exercise.map(name => <Exercise  name={name}/>)
 const putter = tester.map(things => things)
@@ -43,6 +45,7 @@ return (
     <div>{putter}</div>
     {/* <div>{gymBros.musclegroups[0].musclegroup}</div> */}
     {/* <div>{renderlist}</div> */}
+    {/* <MuscleGroups musclegroups={gymBros.musclegroups} /> */}
     
   </div>
   );
