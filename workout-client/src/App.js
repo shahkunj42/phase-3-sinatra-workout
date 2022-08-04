6128fecfdd0d14d2336c76162a8c2a67ef8b6c11
Zachary <zacharylampi96@gmail.com>
2022-08-04 11:34:01 -0400

    data working

--- workout-client/src/App.js
+++ workout-client/src/App.js
@@ -6,39 +6,44 @@ import MuscleGroups from './musclegroups';
 function App() {
 const [count, setCount] = useState(1);
 const [gymBros, setGymBros] = useState([])
+const [tester, setTester] = useState([])
 
 
 const increment = () => {
-  setCount((prevCount) => prevCount + 1);
+  if (count<6) {setCount((prevCount) => prevCount + 1)}
+  else {setCount(1)}
 };
 
 useEffect(() => {
   fetch(`http://localhost:9292/GymBros/${count}`)
   .then(r => r.json())
   .then(data => setGymBros(data))
-},[])
+},[count])
 
 // useEffect(() => {
 //   let renderMuscles = <MuscleGroups musclegroups = {gymBros.musclegroups}/>
 
 //   return renderMuscles
 // }, [gymBros])
+useEffect(()=> {setTimeout(() => setTester(gymBros.musclegroups[0].exercise.map( things => things.exercise)
+  ), 1000);},[gymBros])
 
-setTimeout(() => console.log(gymBros.musclegroups[0].exercise.map( things => things.exercise)
-), 0);
 
+// setTimeout(() => {return (gymBros.musclegroups[0].exercise.map( things => things.exercise)
+// )}, 5000);
 console.log(gymBros)
-
+console.log(tester)
 
 // const renderlist = gymBros.musclegroups[0].exercise.map(name => <Exercise  name={name}/>)
-
+const putter = tester.map(things => things)
 return (
   <div>
     <button onClick={increment}>Next Gym Rat{count}</button>
     <div>{gymBros.name}</div>
-    {/* <div>{gymBros.musclegroups[0].musclegroup}</div>
-    <div>{renderlist}</div> */}
-    {/* {renderMuscles} */}
+    <div>{putter}</div>
+    {/* <div>{gymBros.musclegroups[0].musclegroup}</div> */}
+    {/* <div>{renderlist}</div> */}
+    
   </div>
   );
 }
